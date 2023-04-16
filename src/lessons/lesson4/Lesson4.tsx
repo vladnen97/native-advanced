12793585e4e0f271859f57cd5df67f1b345d3365
vladnen97 <darkdescent1991@gmail.com>
2023-04-16 15:36:52 +0300

    lesson4: promises

--- src/lessons/lesson4/Lesson4.tsx
+++ src/lessons/lesson4/Lesson4.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './lesson_4';
+import {createPromiseHandler, rejectPromiseHandler, resolvePromiseHandler} from './lesson_4';
 
 const Lesson4 = () => {
     return (
         <div>
-            <button id='btn-create-promise'>Create Promise</button>
-            <button id='btn-resolve-promise'>Resolve Promise</button>
-            <button id='btn-reject-promise'>Reject Promise</button>
+            <button id='btn-create-promise' onClick={createPromiseHandler}>Create Promise</button>
+            <button id='btn-resolve-promise' onClick={resolvePromiseHandler}>Resolve Promise</button>
+            <button id='btn-reject-promise' onClick={rejectPromiseHandler}>Reject Promise</button>
         </div>
     );
 }
--- src/lessons/lesson4/lesson_4.ts
+++ src/lessons/lesson4/lesson_4.ts
@@ -1,4 +1,3 @@
-console.log('lesson 4');
 
 // http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
 // https://jakearchibald.com/2015/tasks-microtasks-queues-and-schedules/
@@ -8,18 +7,28 @@ console.log('lesson 4');
 // Создайте промис, который постоянно находиться в состоянии pending.
 // В конструкторе промиса выведите в консоль сообщение "Promise is created".
 
+new Promise((res, rej) => {
+    console.log('Promise is created')
+})
+
 
 // Task 02
 // Создайте промис, который после создания сразу же переходит в состояние resolve
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
 
+Promise.resolve('Promise Data').then((data) => {
+    console.log(data);
+})
 
 // Task 03
 // Создайте промис, который после создания сразу же переходит в состояние rejected
 // и возвращает строку 'Promise Error'
 // Получите данные промиса и выведите их в консоль
 
+Promise.reject('Promise Error').catch(error => {
+    console.log(error)
+})
 
 // Task 04
 // Создайте промис, который переходит в состояние resolved через 3с.
@@ -27,6 +36,13 @@ console.log('lesson 4');
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
 
+new Promise((res, rej) => {
+    setTimeout(() => {
+        res('Promise Data')
+    }, 3000)
+}).then(data => {
+    console.log(data)
+})
 
 // Task 05
 // Создайте литерал объекта handlePromise со следующими свойствами:
@@ -41,6 +57,40 @@ console.log('lesson 4');
 // свойства resolve и reject получают ссылки на соответствующие функции
 // resolve и reject. Следующие два обработчика запускают методы resolve и reject.
 
+type HandlePromiseType = {
+    promise: null | Promise<any>
+    resolve: null | ((value?: any) => void)
+    reject : null | ((reason?: any) => void)
+    onSuccess: (value: string)=> void
+    onError: (value: string)=> void
+}
+
+const handlePromise: HandlePromiseType = {
+    promise: null,
+    resolve: null,
+    reject : null,
+    onSuccess(value: string) {console.log(`Promise is resolved with data: ${value}`)},
+    onError(value: string)   {console.log(`Promise is rejected with error: ${value}`)}
+}
+
+
+export const createPromiseHandler = () => {
+    handlePromise.promise = new Promise((res, rej) => {
+        handlePromise.resolve = res
+        handlePromise.reject = rej
+    })
+    handlePromise.promise?.then(handlePromise.onSuccess).catch(handlePromise.onError)
+}
+
+export const resolvePromiseHandler = () => {
+    handlePromise.resolve?.('RESOLVE')
+}
+
+export const rejectPromiseHandler = () => {
+    handlePromise.reject?.('REJECT')
+
+}
+
 
 // Task 06
 // Создайте промис, который через 1 с возвращает строку "My name is".
@@ -48,6 +98,19 @@ console.log('lesson 4');
 // прибавляет к нему Ваше имя и возвращает новую строку из функции
 // Создайте функцию print, которая выводит в консоль значение своего параметра
 // Добавьте два метода then и передайте созданные функции.
+
+function onSuccess(value: string | unknown): string {
+    return value + ' Vlad'
+}
+
+function print(value: string) {
+    console.log(value)
+}
+
+new Promise((res) => {
+    setTimeout(() => res('My name is'), 1000)
+}).then(onSuccess)
+    .then(print)
 
 
 // Task 7
@@ -57,6 +120,13 @@ console.log('lesson 4');
 // и выведите в консоль {name, age, city}
 
 
+const result = async () => {
+    const pr1: any = await new Promise(res => {setTimeout(()=> res({ name: "Anna" }), 2000)})
+    const pr2: any = await new Promise(res => {setTimeout(()=> res({age: 16}), 3000)})
+    const pr3: any = await new Promise(res => {setTimeout(()=> res({city: 'Moscow'}), 4000)})
 
-// just a plug
-export default ()=>{};
+    return {...pr1, ...pr2, ...pr3}
+}
+
+result().then(console.log)
+
