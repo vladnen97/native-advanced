f3f13f2562a3d8e312295132deafae6566a409c4
vladnen97 <darkdescent1991@gmail.com>
2023-04-22 23:35:27 +0300

    lesson3: get post put delete methods

--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -1,4 +1,4 @@
-console.log('lesson 3');
+import axios from 'axios';
 
 // Event loop
 // https://learn.javascript.ru/event-loop
@@ -15,6 +15,28 @@ console.log('lesson 3');
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
 
+const instance = axios.create(
+    {
+        baseURL: 'https://jsonplaceholder.typicode.com/',
+    })
+
+const postAPI = {
+    getPosts() {
+        return instance.get(`posts`).then(res => console.log(res.data))
+    },
+    getPostById(id: number) {
+        return instance.get(`posts/${id}`).then(res => console.log(res.data))
+    },
+    createPost(body: {  title: string, body: string, userId: number}) {
+        return instance.post(`posts`, {...body}).then(res => console.log(res.data))
+    },
+    updatePostById(postId: number, body: {  title: string, body: string, userId: number}) {
+        return instance.put(`posts/${postId}`, {...body}).then(res => console.log(res.data))
+    },
+    deletePostById(postId: number) {
+        return instance.delete(`posts/${postId}`).then(res => console.log(res.data))
+    }
+}
 
 // just a plug
 export default ()=>{};
