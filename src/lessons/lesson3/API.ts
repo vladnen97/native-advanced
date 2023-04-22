e0340d0cc76c0db7b11f8d005e6fe78479d5cf32
vladnen97 <darkdescent1991@gmail.com>
2023-04-22 23:00:25 +0300

    lesson3: api requests

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -3,13 +3,15 @@ import axios from 'axios';
 const configOMB = {
     baseURL: 'http://www.omdbapi.com',
 };
-const key = '';
+const key = 'e8a0edb1';
 const axiosInstance = axios.create(configOMB);
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+        return axiosInstance.get(`/?apikey=${key}&t=${title}`).then(res => res.data)
     },
     searchFilmsByType: (title: string, type: string) => {
+        return axiosInstance.get(`/?apikey=${key}&t=${title}&type=${type}`).then(res => res.data)
     }
 };
 
